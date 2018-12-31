4e320df037b790c607d81a63fb1c747314b1843e
Pierrot <pierrot@Pierrots-MacBook.local>
2018-12-31 17:07:20 +0900

    dotfilesをダウンロードする処理を抜き出した。

--- setup.sh
+++ setup.sh
@@ -3,16 +3,22 @@
 # dotfiles path
 DOTFILES_PATH="$HOME/dotfiles"
 
-if [ -d $DOTFILES_PATH ]; then
-	echo "dotfiles: already exists"
-else
-	echo "Downloading dotfiles..."
-	if type git > /dev/null 2>&1; then
-		git clone https://github.com/Pierrotica/dotfiles.git
+download_dotfiles() {
+	if [ -d $DOTFILES_PATH ]; then
+		echo "dotfiles: already exists"
 	else
-		curl -sL https://github.com/Pierrotica/dotfiles/archive/master.tar.gz | tarxz
-		mv dotfiles-master dotfiles
+		echo "Downloading dotfiles..."
+		if type git > /dev/null 2>&1; then
+			git clone https://github.com/Pierrotica/dotfiles.git
+		else
+			curl -sL https://github.com/Pierrotica/dotfiles/archive/master.tar.gz | tarxz
+			mv dotfiles-master dotfiles
+		fi
+		echo "successfully downloaded"
 	fi
-	echo "successfully downloaded"
-fi
-"
+}
+
+main () {
+	download_dotfiles
+}
+main
