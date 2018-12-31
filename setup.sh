5ea6fd57c35ddacc07dde7808d8f64cc184384ac
Pierrot <pierrot@Pierrots-MacBook.local>
2018-12-31 20:07:04 +0900

    dotfilesをダウンロードしてからでないとutils.shを読み込めないため、 使わないように変更した。

--- setup.sh
+++ setup.sh
@@ -3,30 +3,27 @@
 # dotfiles path
 DOTFILES_PATH="$HOME/dotfiles"
 
-# Load utils
-. $DOTFILES_PATH/setup/utils.sh
-
 check_os() {
 	os_name="$(uname)"
 	if [ "$os_name" != "Darwin" ]; then
-		print_error "Sorry, this script is intended only for macOS"
+		echo "Sorry, this script is intended only for macOS"
 		exit 1
 	fi
 }
 
 download_dotfiles() {
-	print_title "Download dotfiles"
+	echo "Download dotfiles"
 	if [ -d $DOTFILES_PATH ]; then
-		print_warning "dotfiles: already exists"
+		echo "dotfiles: already exists"
 	else
-		print_message "Downloading dotfiles..."
+		echo "Downloading dotfiles..."
 		if type git > /dev/null 2>&1; then
 			git clone https://github.com/Pierrotica/dotfiles.git
 		else
 			curl -sL https://github.com/Pierrotica/dotfiles/archive/master.tar.gz | tarxz
 			mv dotfiles-master dotfiles
 		fi
-		print_success "successfully downloaded"
+		echo "successfully downloaded"
 	fi
 }
 
